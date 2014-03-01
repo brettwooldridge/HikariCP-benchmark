baf1e1673015f41a66d9554703f8c42223f2d4ac
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-03-01 21:46:42 +0900

    Add "medium" size benchmark run.

--- benchmark.sh
+++ benchmark.sh
@@ -6,6 +6,8 @@ fi
 
 if [[ "quick" == "$1" ]]; then
    java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -wi 8 -i 8 -t 8 -f 2 $2 $3 $4 $5 $6 $7 $8 $9
+elif [[ "medium" == "$1" ]]; then
+   java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -t 8 -f 3 $2 $3 $4 $5 $6 $7 $8 $9
 elif [[ "profile" == "$1" ]]; then
    java -server -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=8849 -jar ./target/microbenchmarks.jar -wi 8 -i 8 -t 8 -f 0 $2 $3 $4 $5 $6 $7 $8 $9
 else
