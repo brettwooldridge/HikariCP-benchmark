d97eefe22a4617ceac99608f53525a0507f33792
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-03-02 19:18:38 +0900

    Change benchmark properties for profiling and full-run testing.

--- benchmark.sh
+++ benchmark.sh
@@ -9,7 +9,7 @@ if [[ "quick" == "$1" ]]; then
 elif [[ "medium" == "$1" ]]; then
    java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -t 8 -f 3 $2 $3 $4 $5 $6 $7 $8 $9
 elif [[ "profile" == "$1" ]]; then
-   java -server -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=8849 -jar ./target/microbenchmarks.jar -wi 8 -i 8 -t 8 -f 0 $2 $3 $4 $5 $6 $7 $8 $9
+   java -server -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=8849 -jar ./target/microbenchmarks.jar -r 5 -wi 8 -i 8 -t 8 -f 0 $2 $3 $4 $5 $6 $7 $8 $9
 else
-   java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -wi 10 -i 10 -t 8 $2 $3 $4 $5 $6 $7 $8 $9
+   java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -wi 15 -i 10 -t 8 $2 $3 $4 $5 $6 $7 $8 $9
 fi
