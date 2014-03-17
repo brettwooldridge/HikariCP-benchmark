1924c9186fe089516ea39157f757ae1f7412bb4f
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-03-17 14:55:36 +0900

    Add missing parameter to benchmark wrapper

--- benchmark.sh
+++ benchmark.sh
@@ -11,5 +11,5 @@ elif [[ "medium" == "$1" ]]; then
 elif [[ "profile" == "$1" ]]; then
    java -server -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=8849 -jar ./target/microbenchmarks.jar -r 5 -wi 8 -i 8 -t 8 -f 0 $2 $3 $4 $5 $6 $7 $8 $9
 else
-   java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -wi 15 -i 10 -t 8 $2 $3 $4 $5 $6 $7 $8 $9
+   java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -wi 15 -i 10 -t 8 $1 $2 $3 $4 $5 $6 $7 $8 $9
 fi
