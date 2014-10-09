cd9056f25f555a328d0c0dfa5851a6061107ef1b
Brett Wooldridge <brett.wooldridge@gmail.com>
2014-10-09 15:59:28 +0900

    Minor tweaks to allow faster test runs.

--- benchmark.sh
+++ benchmark.sh
@@ -5,13 +5,13 @@ if [[ ! -e "./target/microbenchmarks.jar" ]]; then
 fi
 
 if [[ "quick" == "$1" ]]; then
-   java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -wi 8 -i 8 -t 8 -f 2 $2 $3 $4 $5 $6 $7 $8 $9
+   java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -wi 3 -i 8 -t 8 -f 2 $2 $3 $4 $5 $6 $7 $8 $9
 elif [[ "medium" == "$1" ]]; then
-   java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -t 8 -f 3 $2 $3 $4 $5 $6 $7 $8 $9
+   java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -wi 3 -t 8 -f 3 $2 $3 $4 $5 $6 $7 $8 $9
 elif [[ "profile" == "$1" ]]; then
-   java -server -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=8849 -jar ./target/microbenchmarks.jar -r 5 -wi 8 -i 8 -t 8 -f 0 $2 $3 $4 $5 $6 $7 $8 $9
+   java -server -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=8849 -jar ./target/microbenchmarks.jar -r 5 -wi 3 -i 8 -t 8 -f 0 $2 $3 $4 $5 $6 $7 $8 $9
 elif [[ "debug" == "$1" ]]; then
-   java -server -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y -jar ./target/microbenchmarks.jar -r 5 -wi 8 -i 8 -t 8 -f 0 $2 $3 $4 $5 $6 $7 $8 $9
+   java -server -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y -jar ./target/microbenchmarks.jar -r 5 -wi 3 -i 8 -t 8 -f 0 $2 $3 $4 $5 $6 $7 $8 $9
 else
-   java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -wi 15 -i 10 -t 8 $1 $2 $3 $4 $5 $6 $7 $8 $9
+   java -jar ./target/microbenchmarks.jar -jvmArgs "-server" -wi 3 -i 15 -t 8 $1 $2 $3 $4 $5 $6 $7 $8 $9
 fi
