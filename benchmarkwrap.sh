38af6d7e0bce40684179aca83aad4846a93a0d0a
Kenny Gryp <gryp@dakin.be>
2014-11-02 10:36:58 +0000

    back to all choices

--- benchmarkwrap.sh
+++ benchmarkwrap.sh
@@ -1,4 +1,4 @@
 for threads in 1 2 4 8 16 32
 do
-	./benchmark.sh medium $threads -p pool=c3p0-ht6 -p maxPoolSize=1,2,4,8,16,32 ".*Trx.*"
+	./benchmark.sh medium $threads -p pool=c3p0,hikari,jdbc,dbcp,c3p0-ht6 -p maxPoolSize=1,2,4,8,16,32 ".*Trx.*"
 done
