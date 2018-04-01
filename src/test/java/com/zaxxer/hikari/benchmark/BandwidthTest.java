517a90cf32d9f6ec539b654194715bddc3909fb9
Brett Wooldridge <brett.wooldridge@gmail.com>
2018-04-02 01:25:45 +0900

    bandwidth test

--- src/test/java/com/zaxxer/hikari/benchmark/BandwidthTest.java
+++ src/test/java/com/zaxxer/hikari/benchmark/BandwidthTest.java
@@ -1,6 +1,9 @@
 package com.zaxxer.hikari.benchmark;
 
+import static com.zaxxer.hikari.util.ClockSource.currentTime;
 import static java.lang.System.out;
+import static java.sql.Connection.TRANSACTION_READ_COMMITTED;
+import static java.sql.Connection.TRANSACTION_READ_UNCOMMITTED;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -23,14 +26,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vibur.dbcp.ViburDBCPDataSource;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import com.zaxxer.hikari.util.ClockSource;
 
 public class BandwidthTest
 {
-   private static final Logger LOGGER = LoggerFactory.getLogger(SpikeLoadTest.class);
+   private static final Logger LOGGER = LoggerFactory.getLogger(BandwidthTest.class);
 
    private static final String jdbcUrl = "jdbc:postgresql://localhost/test";
 
@@ -46,7 +48,8 @@ public class BandwidthTest
 
       test.setup(args);
 
-      out.println("Blackhole value " + test.start());
+      int blackhole = test.start();
+      LOGGER.debug("Blackhole value {}", blackhole);
    }
 
    private void setup(String[] args) throws SQLException
@@ -59,9 +62,6 @@ public class BandwidthTest
       case "dbcp2":
          setupDbcp2();
          break;
-      case "c3p0":
-         setupC3P0();
-         break;
       case "tomcat":
          setupTomcat();
          break;
@@ -73,57 +73,46 @@ public class BandwidthTest
       }
 
       try (Connection connection = DS.getConnection()) {
-         out.printf("Primed connection: %s.", pool);
          connection.setAutoCommit(true);
-         Statement stmt = connection.createStatement();
-         stmt.execute("DROP TABLE IF EXISTS test; CREATE TABLE test (value VARCHAR(255));");
-         stmt.executeUpdate("INSERT INTO test (value) VALUES ('this is a test')");
-         connection.setAutoCommit(false);
+         try (Statement stmt = connection.createStatement()) {
+            stmt.execute("DROP TABLE IF EXISTS test; CREATE TABLE test (value VARCHAR(255));");
+            stmt.executeUpdate("INSERT INTO test (value) VALUES ('this is a test')");
+            connection.setAutoCommit(false);
+         }
       }
+
+      LOGGER.info("Primed connection: {}.", pool);
    }
 
    private int start() throws InterruptedException, SQLException
    {
-      int blackhole = 0;
       try (Scanner scanner = new Scanner(System.in)) {
-
-         out.println("\n[Re]start iptop capture and press enter: sudo iftop -i lo0 -nf \"port 5432 and host localhost\"");
+         LOGGER.info("\n[Re]start iptop capture and press enter: sudo iftop -i lo0 -nf \"port 5432 and host localhost\"");
          scanner.nextLine();
 
-         long start = ClockSource.currentTime();
-         for (int i = 0; i < 10000; i++) {
+         int blackhole = 0;
+         long start = currentTime();
+
+         for (int i = 0; i < 10_000; i++) {
             try (Connection connection = DS.getConnection()) {
-               connection.setReadOnly(true);
-               if (!connection.isReadOnly()) {
-                  LOGGER.warn("Read-only was not reset to 'true'");
+               // connection.setTransactionIsolation(TRANSACTION_READ_UNCOMMITTED);
+               // connection.setReadOnly(true);
+               try (Statement stmt = connection.createStatement();
+                    ResultSet rs = stmt.executeQuery("SELECT * FROM test")) {
+                  if (rs.next()) {
+                     blackhole += rs.getString(1).hashCode();
+                  }
                }
-               connection.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED);
-               Statement stmt = connection.createStatement();
-               ResultSet rs = stmt.executeQuery("SELECT * FROM test");
-               if (rs.next()) {
-                  blackhole += rs.getString(1).hashCode();
-               }
+               // connection.rollback();
             }
          }
 
-         out.println("Elapsed runtime " + ClockSource.elapsedDisplayString(start, ClockSource.currentTime()));
-         out.flush();
-
-         try (Connection connection = DS.getConnection()) {
-            if (connection.isReadOnly()) {
-               LOGGER.warn("Read-only was not reset to 'false'");
-            }
-
-            if (connection.getTransactionIsolation() != Connection.TRANSACTION_READ_COMMITTED) {
-               LOGGER.warn("Transaction isolation level was not reset to TRANSACTION_READ_COMMITTED");
-            }
-         }
+         LOGGER.info("Elapsed runtime {}" , ClockSource.elapsedDisplayString(start, currentTime()));         
+         return blackhole;
       }
       finally {
          shutdownPool(DS);
       }
-
-      return blackhole;
    }
 
    private void shutdownPool(DataSource ds)
@@ -135,37 +124,6 @@ public class BandwidthTest
          catch (Exception e) {
          }
       }
-      else if (ds instanceof ComboPooledDataSource) {
-         ((ComboPooledDataSource) ds).close();
-      }
-   }
-
-   private void setupDbcp2()
-   {
-      BasicDataSource ds = new DbcpPoolAccessor();
-      ds.setUrl(jdbcUrl);
-      ds.setUsername("brettw");
-      ds.setPassword("");
-      ds.setInitialSize(MIN_POOL_SIZE);
-      ds.setMinIdle(MIN_POOL_SIZE);
-      ds.setMaxIdle(MAX_POOL_SIZE);
-      ds.setMaxTotal(MAX_POOL_SIZE);
-      ds.setMaxWaitMillis(8000);
-
-      ds.setSoftMinEvictableIdleTimeMillis(MINUTES.toMillis(10));
-      ds.setTimeBetweenEvictionRunsMillis(SECONDS.toMillis(30));
-
-      ds.setDefaultAutoCommit(false);
-      ds.setDefaultTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-      ds.setDefaultReadOnly(false);
-
-      ds.setRollbackOnReturn(true);
-      ds.setEnableAutoCommitOnReturn(false);
-      ds.setTestOnBorrow(true);
-      ds.setCacheState(true);
-      ds.setFastFailValidation(true);
-
-      DS = ds;
    }
 
    private void setupHikari()
@@ -184,6 +142,34 @@ public class BandwidthTest
       DS = new HikariDataSource(config);
    }
 
+   private void setupDbcp2()
+   {
+      BasicDataSource ds = new DbcpPoolAccessor();
+      ds.setUrl(jdbcUrl);
+      ds.setUsername("brettw");
+      ds.setPassword("");
+      ds.setInitialSize(MIN_POOL_SIZE);
+      ds.setMinIdle(MIN_POOL_SIZE);
+      ds.setMaxIdle(MIN_POOL_SIZE);
+      ds.setMaxTotal(MAX_POOL_SIZE);
+      ds.setMaxWaitMillis(8000);
+
+      ds.setSoftMinEvictableIdleTimeMillis(MINUTES.toMillis(10));
+      ds.setTimeBetweenEvictionRunsMillis(SECONDS.toMillis(30));
+
+      ds.setDefaultAutoCommit(false);
+      ds.setDefaultTransactionIsolation(TRANSACTION_READ_COMMITTED);
+      ds.setDefaultReadOnly(false);
+
+      ds.setRollbackOnReturn(true);
+      ds.setEnableAutoCommitOnReturn(false);
+      ds.setTestOnBorrow(true);
+      ds.setCacheState(true);
+      ds.setFastFailValidation(true);
+
+      DS = ds;
+   }
+
    protected void setupTomcat()
    {
        PoolProperties props = new PoolProperties();
@@ -198,7 +184,7 @@ public class BandwidthTest
 
        props.setDefaultAutoCommit(false);
        props.setDefaultReadOnly(false);
-       props.setDefaultTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+       props.setDefaultTransactionIsolation(TRANSACTION_READ_COMMITTED);
 
        props.setRollbackOnReturn(true);
        props.setUseDisposableConnectionFacade(true);
@@ -224,29 +210,6 @@ public class BandwidthTest
        DS = new TomcatPoolAccessor(props);
    }
 
-   private void setupC3P0()
-   {
-      try {
-         ComboPooledDataSource cpds = new ComboPooledDataSource();
-         cpds.setJdbcUrl(jdbcUrl);
-         cpds.setUser("brettw");
-         cpds.setPassword("");
-         cpds.setAcquireIncrement(1);
-         cpds.setInitialPoolSize(MIN_POOL_SIZE);
-         cpds.setNumHelperThreads(2);
-         cpds.setMinPoolSize(MIN_POOL_SIZE);
-         cpds.setMaxPoolSize(MAX_POOL_SIZE);
-         cpds.setCheckoutTimeout(8000);
-         cpds.setLoginTimeout(8);
-         cpds.setTestConnectionOnCheckout(true);
-
-         DS = cpds;
-      }
-      catch (Exception e) {
-         throw new RuntimeException(e);
-      }
-   }
-
    private void setupVibur()
    {
       ViburDBCPDataSource vibur = new ViburDBCPDataSource();
@@ -260,11 +223,11 @@ public class BandwidthTest
       vibur.setUseNetworkTimeout(true);
       vibur.setNetworkTimeoutExecutor(Executors.newFixedThreadPool(1));
       vibur.setClearSQLWarnings(true);
-      vibur.setResetDefaultsAfterUse(true);
 
       vibur.setDefaultAutoCommit(false);
       vibur.setDefaultReadOnly(false);
-      vibur.setDefaultTransactionIsolationValue(Connection.TRANSACTION_READ_COMMITTED);
+      vibur.setDefaultTransactionIsolationValue(TRANSACTION_READ_COMMITTED);
+      vibur.setResetDefaultsAfterUse(true);
 
       vibur.setReducerTimeIntervalInSeconds((int) MINUTES.toSeconds(10));
 
